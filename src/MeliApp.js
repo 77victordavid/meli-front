7f8af5f33d3f9975f626d5de517725668b51cf10
Victor David <vdavid@proteccion.com.co>
2022-04-09 12:07:06 -0500

    feat: Maquetacion card para resultados de busqueda, primer item

--- src/MeliApp.js
+++ src/MeliApp.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { AppRouter } from "./routers/AppRouter";
+import './MeliApp.scss';
 
 export const MeliApp = () => {
   return (
-    <>
+    <div className="container-meli">
       <AppRouter />
-    </>
+    </div>
   );
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import "./Home.scss";
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container home">
+      <div className="container-items d-flex">
+        {/* <div className='row'>
+          <div className='col-2'>
+            <img src='https://http2.mlstatic.com/D_NQ_NP_691353-MCO49533588258_032022-W.webp'/>
+          </div>
+          <div className='col-8'>
+            <h3>$1.980</h3>
+            <p>Apple ipod toour 5g 16gb negro igual a nuevo completo unico!</p>
+          </div>
+          <div className='col-2'>
+            <p>Capital federal</p>
+          </div>
+        </div> */}
+        <img
+          className="img-product"
+          src="https://http2.mlstatic.com/D_NQ_NP_691353-MCO49533588258_032022-W.webp"
+        />
+        <div className="item-description">
+          <div className="item-description-price">
+            <h4>$1.980</h4>
+            <div className="item-description-icon-shipping"></div>
+          </div>
+          <p>Apple ipod toour 5g 16gb negro igual a nuevo completo unico!</p>
+        </div>
+        <p className="item-description-city">Capital federal</p>
+      </div>
+    </div>
+  );
+};
